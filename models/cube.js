00b99be1349ba45b8602ca13ea47e0ce93be8cef
vsterev <vsterev@gmail.com>
2020-06-13 13:48:17 +0300

    changhe cube model to handel createId

--- models/cube.js
+++ models/cube.js
@@ -4,7 +4,8 @@ const cubeSchema = new mongoose.Schema({
     description: { type: String, maxlength: 2000 } || 'No description',
     imageUrl: { type: String } || 'https://www.imghack/com/id?389872',
     difficulty: { type: Number, min:1, max:6 },
-    accessories: [{ type: mongoose.Types.ObjectId, ref: 'Accessories' }]
+    accessories: [{ type: mongoose.Types.ObjectId, ref: 'Accessories' }],
+    createrId: { type: mongoose.Types.ObjectId, ref: 'User' }
 })
 
 module.exports = mongoose.model('Cube', cubeSchema);
